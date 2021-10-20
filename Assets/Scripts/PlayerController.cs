159ac5427c2cf03afd424a1dd135571b7b4767d8
José Ruiz <joseruiz@keemail.me>
2021-10-20 04:46:38 +0200

    Add player object

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float force = 300.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
