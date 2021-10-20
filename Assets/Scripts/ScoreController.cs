29b416b517ecbeafb77f36eabb61a6f7c6cf7b2c
José Ruiz <joseruiz@keemail.me>
2021-10-20 07:22:20 +0200

    Add score board

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoreText;
+    public int scorePoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPoint()
+    {
+        scorePoints++;
+        scoreText.text = "Score: " + scorePoints.ToString();
+    }
+}
